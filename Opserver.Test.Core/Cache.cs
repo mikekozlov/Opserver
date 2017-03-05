d2146d5c84aa8f49ea7d38dcb05878118735aa04
mikekozlov <mykhail.kozlov@gmail.com>
2017-03-05 22:18:12 +0200

    Cache object enhancnments

--- Opserver.Test.Core/Cache.cs
+++ Opserver.Test.Core/Cache.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Authentication;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using NLog;
 
@@ -9,29 +11,56 @@ namespace Opserver.Test.Core
 {
     public class Cache<T> : Cache where T : class, new()
     {
-        Func<Task<T>> _updateCache;
+        private int _hasData;
+
+        private Func<Task<T>> _updateCache;
 
         public T Data { get; set; }
 
         public Task<T> DataTask { get; set; }
 
 
-        public Cache()
+        public Cache(string key, Func<Task<T>> getData)
         {
+            Key = key;
+
             _updateCache = async ()
              =>
             {
                 Current.Logger.Trace($"Update Cache [{Key}] Started.");
 
+                var data = await getData();
 
+                IsStale = false;
+                IsPolling = false;
 
                 Current.Logger.Trace($"Update Cache [{Key}] Completed.");
 
-                return await Task.FromResult(new T());
+                return data;
             };
         }
 
+        public void Update()
+        {
+            try
+            {
+                DataTask = _updateCache();
 
+            }
+            catch (Exception ex)
+            {
+                Current.Logger.Error(ex, $"Update Cache [{Key}] Failed.");
+            }
+        }
+
+        public Task<int> PollAsync(bool force = false)
+        {
+            if (force || (_hasData == 0 && Interlocked.CompareExchange(ref _hasData, 1, 0) == 0))
+            {
+                
+            }
+
+        }
     }
 
     public abstract class Cache
